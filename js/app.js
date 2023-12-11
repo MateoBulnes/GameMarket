38671ac820030a2b5e9102d8c20fdab6fe3c87d8
MateoBulnes <mbulnes@fi.uba.ar>
2023-12-10 21:27:42 -0300

    Mejora sobre el algoritmo de cálculo de precios

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 var productos_en_carrito = {};
 
-
-
 function agregar_producto(id) {
     let cant_prod_carrito = parseInt(document.getElementById('cant_productos').innerText);
-    document.getElementById('cant_productos').innerText = cant_prod_carrito + 1;
-    productos_en_carrito[id] = formatear_precio(document.getElementById('precio_' + id.split("_")[1]).innerText);
+    let cant_a_agregar = Number(prompt("Ingrese la cantidad de unidades de este juego que desea agregar"));
+    let precio_producto = formatear_precio(document.getElementById('precio_' + id.split("_")[1]).innerText);
+
+    document.getElementById('cant_productos').innerText = cant_prod_carrito + cant_a_agregar;
+    productos_en_carrito[id] = precio_producto * cant_a_agregar;
 }
 
 function formatear_precio(precio_sin_formato) {
@@ -13,24 +14,29 @@ function formatear_precio(precio_sin_formato) {
     return parseInt(nuevo_precio.replace(".", ""));
 }
 
-function calcular_total() {
-    let cant_prod_carrito = parseInt(document.getElementById('cant_productos').innerText);
-    var claves = Object.keys(productos_en_carrito);
-    let total = 0;
-    for (let i = 0; i < cant_prod_carrito; i++) {
-        total += productos_en_carrito[claves[i]];
-    }
-
-    let aviso_descuento = "";
-
-    if (cant_prod_carrito >= 3) {
-        total = aplicar_descuento(total);
-        aviso_descuento += " (Se le aplico un 30% de descuento por comprar más de 2 juegos)";
-    }
-
-    alert("El total en su carrito es de: $" + total + aviso_descuento);
-}
-
 function aplicar_descuento(total_sin_desc) {
     return total_sin_desc * 0.7;
+}
+
+function calcular_total() {
+    let cant_prod_carrito = parseInt(document.getElementById('cant_productos').innerText);
+    if (cant_prod_carrito == 0) {
+        alert("No tiene productos seleccionados en su carrito");
+    } else {
+        //Obtengo las claves de los productos para poder iterar sobre el diccionario
+        let claves = Object.keys(productos_en_carrito);
+        let total = 0;
+        for (let i = 0; i < claves.length; i++) {
+            total += productos_en_carrito[claves[i]];
+        }
+
+        let aviso_descuento = "";
+
+        if (cant_prod_carrito >= 3) {
+            total = aplicar_descuento(total);
+            aviso_descuento += " (Se le aplico un 30% de descuento por comprar más de 2 juegos)";
+        }
+
+        alert("El total en su carrito es de: $" + total + aviso_descuento);
+    }
 }
