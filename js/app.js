84ffcc65816e7f2aee13822f0797fdbdde426603
MateoBulnes <mbulnes@fi.uba.ar>
2024-01-07 11:17:18 -0300

    Modificacion carrito como array de objetos + funcionalidad ver carrito

--- js/app.js
+++ js/app.js
@@ -1,12 +1,19 @@
-var productos_en_carrito = {};
+var productos_carrito = [];
 
 function agregar_producto(id) {
     let cant_prod_carrito = parseInt(document.getElementById('cant_productos').innerText);
     let cant_a_agregar = Number(prompt("Ingrese la cantidad de unidades de este juego que desea agregar"));
-    let precio_producto = formatear_precio(document.getElementById('precio_' + id.split("_")[1]).innerText);
 
     document.getElementById('cant_productos').innerText = cant_prod_carrito + cant_a_agregar;
-    productos_en_carrito[id] = precio_producto * cant_a_agregar;
+    let producto = construir_producto(id, cant_a_agregar);
+    productos_carrito.push(producto);
+}
+
+function construir_producto(id_producto, cantidad_prod) {
+    let nombre_prod = document.getElementById('nombre_prod_' + id_producto.split("_")[1]).innerText;
+    let precio_prod = formatear_precio(document.getElementById('precio_' + id_producto.split("_")[1]).innerText);
+
+    return { nombre: nombre_prod, precio: precio_prod, cantidad: cantidad_prod };
 }
 
 function formatear_precio(precio_sin_formato) {
@@ -18,16 +25,26 @@ function aplicar_descuento(total_sin_desc) {
     return total_sin_desc * 0.7;
 }
 
+function cantidad_carrito() {
+    let cantidad_carrito = 0;
+    
+    for (let i = 0; i < productos_carrito.length; i++) {
+        console.log(productos_carrito[i].cantidad)
+        cantidad_carrito += productos_carrito[i].cantidad;
+    }
+
+    return cantidad_carrito;
+}
+
 function calcular_total() {
-    let cant_prod_carrito = parseInt(document.getElementById('cant_productos').innerText);
+    let cant_prod_carrito = cantidad_carrito();
+
     if (cant_prod_carrito == 0) {
         alert("No tiene productos seleccionados en su carrito");
     } else {
-        //Obtengo las claves de los productos para poder iterar sobre el diccionario
-        let claves = Object.keys(productos_en_carrito);
         let total = 0;
-        for (let i = 0; i < claves.length; i++) {
-            total += productos_en_carrito[claves[i]];
+        for (let i = 0; i < productos_carrito.length; i++) {
+            total += productos_carrito[i].precio * productos_carrito[i].cantidad;
         }
 
         let aviso_descuento = "";
@@ -39,4 +56,14 @@ function calcular_total() {
 
         alert("El total en su carrito es de: $" + total + aviso_descuento);
     }
+}
+
+function ver_carrito() {
+    let subtotal = 0;
+    console.log("Productos en el carrito: ");
+    for (let i = 0; i < productos_carrito.length; i++) {
+        console.log(`${i + 1}: ${productos_carrito[i].nombre}, $${productos_carrito[i].precio} x ${productos_carrito[i].cantidad} `);
+        subtotal += productos_carrito[i].precio * productos_carrito[i].cantidad;
+    }
+    console.log(`Subtotal: $${subtotal}`);
 }
